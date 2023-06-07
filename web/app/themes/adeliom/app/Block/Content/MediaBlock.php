dfd4c0163da9947217249e2ba1809c0646158103
Davy Jacquemard <81741891+djacquemard@users.noreply.github.com>
2023-06-07 11:50:33 +0200

    feat(blocks): add media blocks (#36)
    * feat(blocks): add media blocks
    feat(modal): add modal library and setup
    feat(lightbox): add lightbox library and setup
    
    * feat(assets): refactor assets structure + webpack encore config

---
+++ web/app/themes/adeliom/app/Block/Content/MediaBlock.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Block\Content;
+
+use Adeliom\Lumberjack\Admin\AbstractBlock;
+use Adeliom\Lumberjack\Admin\Fields\Buttons\ButtonField;
+use Adeliom\Lumberjack\Admin\Fields\Choices\TrueFalseField;
+use Adeliom\Lumberjack\Admin\Fields\Layout\LayoutField;
+use Adeliom\Lumberjack\Admin\Fields\Medias\MediaField;
+use Adeliom\Lumberjack\Admin\Fields\Tabs\ContentTab;
+use Adeliom\Lumberjack\Admin\Fields\Tabs\LayoutTab;
+use Adeliom\Lumberjack\Admin\Fields\Tabs\MediaTab;
+use Adeliom\Lumberjack\Admin\Fields\Typography\HeadingField;
+use Adeliom\Lumberjack\Admin\Fields\Typography\WysiwygField;
+use Adeliom\Lumberjack\Assets\Assets;
+use App\Enum\BlocksTwigPath;
+use App\Enum\GutBlockName;
+use Extended\ACF\ConditionalLogic;
+use Extended\ACF\Fields\RadioButton;
+
+/**
+ * Class MediaBlock
+ * @see https://github.com/wordplate/extended-acf#fields
+ * @package App\FlexibleLayout
+ */
+class MediaBlock extends AbstractBlock
+{
+    public const NAME = "media";
+    public const TITLE = "Média";
+    public const DESCRIPTION = "Bloc média";
+
+    public function __construct()
+    {
+        parent::__construct([
+            'mode' => 'edit',
+            'category' => GutBlockName::CONTENT,
+            'dir' => BlocksTwigPath::CONTENT,
+            'supports' => [
+                "anchor" => true
+            ],
+            'enqueue_assets' => function () {
+                Assets::enqueue('scripts/blocks/media', 'scripts/blocks/media', []);
+            },
+        ]);
+    }
+
+    public static function getFields(): ?\Traversable
+    {
+
+        yield from ContentTab::make()->fields([
+            HeadingField::make()->tag(),
+        ]);
+
+        yield from MediaTab::make()->fields([
+            MediaField::make()
+        ]);
+
+        yield from LayoutTab::make()->fields([
+            TrueFalseField::make("Contraindre le ratio du média", "has_ratio"),
+            RadioButton::make("Ratio", "ratio")->choices([
+                "auto" => "Automatique",
+                "paysage" => "Paysage",
+                "portrait" => "Portrait"
+            ])->conditionalLogic([
+                ConditionalLogic::where("has_ratio", "==", 1)
+            ]),
+            LayoutField::margin()
+        ]);
+    }
+}
---
+++ web/app/themes/adeliom/assets/scripts/blocks/media.ts
@@ -0,0 +1,37 @@
+import "glightbox/dist/css/glightbox.css";
+import GLightbox from "glightbox";
+import { setBackFocus } from "@scripts/components/lightbox";
+
+/*----------------------
+----------------------
+SINGLE VIDEO LIGHTBOX (no gallery)
+Used for :
+- Media block
+- Text + media block
+----------------------
+----------------------*/
+const lightboxVideoSelector = ".js-glightbox-video-single";
+const lightboxVideoGallery = document.querySelector(lightboxVideoSelector);
+
+if (lightboxVideoGallery) {
+    //Custom HTML to disable next and prev button focus
+    const customLightboxHTML = `<div id="glightbox-body" class="glightbox-container">
+    <div class="gloader visible"></div>
+    <div class="goverlay"></div>
+    <div class="gcontainer">
+    <div id="glightbox-slider" class="gslider"></div>
+    <div class="gnext gbtn hidden" aria-label="Next"></div>
+    <div class="gprev gbtn hidden" aria-label="Previous"></div>
+    <button class="gclose gbtn" tabindex="0" aria-label="Close">{closeSVG}</button>
+</div>
+</div>`;
+
+    const lightboxVideo = GLightbox({
+        loop: false,
+        lightboxHTML: customLightboxHTML,
+        selector: lightboxVideoSelector,
+        touchNavigation: false,
+    });
+
+    setBackFocus(lightboxVideoSelector, lightboxVideo);
+}
