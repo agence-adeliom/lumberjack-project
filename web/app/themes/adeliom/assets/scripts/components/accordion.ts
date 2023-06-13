b7856b07503c2e87ed0d7f99d99197456bb46471
hschuhler <hugo.schuhler@adeliom.com>
2023-06-13 09:29:03 +0200

    feat(faq js): init alpine + accordion js

---
+++ web/app/themes/adeliom/assets/scripts/components/accordion.ts
@@ -0,0 +1,40 @@
+import Alpine from "alpinejs";
+document.addEventListener("alpine:init", () => {
+    Alpine.data("initAccordion", () => {
+        const activeClass = "is-active";
+        return {
+            setMaxHeight(content: HTMLElement, height = 0) {
+                content.style.maxHeight = `${height}px`;
+            },
+
+            getContent(index: number) {
+                const container = `container${index}`;
+                return this.$refs[container];
+            },
+
+            closeEl(item: HTMLElement) {
+                const index = item.dataset.index;
+                item.classList.remove(activeClass);
+                this.active = false;
+                const content = this.getContent(index);
+
+                this.setMaxHeight(content);
+            },
+
+            openEl(item: HTMLElement) {
+                const index = item.dataset.index;
+                const content = this.getContent(index);
+                this.active = !this.active;
+                if (item.className.includes(activeClass)) {
+                    item.classList.remove(activeClass);
+                    this.setMaxHeight(content);
+                } else {
+                    const contentHeight = content.scrollHeight;
+                    item.classList.add(activeClass);
+
+                    this.setMaxHeight(content, contentHeight);
+                }
+            },
+        };
+    });
+});
--- web/app/themes/adeliom/assets/scripts/global.ts
+++ web/app/themes/adeliom/assets/scripts/global.ts
@@ -1,1 +1,8 @@
 import "@scripts/navigations/menu";
+
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+document.addEventListener("DOMContentLoaded", () => {
+    Alpine.start();
+});
