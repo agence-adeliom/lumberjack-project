27786b4ad80db218f94d84fc14656bea02b10d03
hschuhler <hugo.schuhler@adeliom.com>
2023-06-16 11:45:25 +0200

    fix(script + twig): little fix function + disable class

--- web/app/themes/adeliom/assets/scripts/blocks/sticky-menu.ts
+++ web/app/themes/adeliom/assets/scripts/blocks/sticky-menu.ts
@@ -1,76 +1,109 @@
-import Swiper, { Navigation } from "swiper";
+import Alpine from "alpinejs";
+import Swiper, { Navigation, SwiperOptions } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-const swiper = new Swiper(".swiper", {
-    // configure Swiper to use modules
-    modules: [Navigation],
-    slidesPerView: "auto",
-    spaceBetween: "24",
-    loop: false,
-    mousewheel: true,
-    navigation: {
-        nextEl: ".js-swiper-next",
-        prevEl: ".js-swiper-prev",
-    },
-});
+document.addEventListener("alpine:init", () => {
+    Alpine.data("initStickyMenu", () => {
+        return {
+            swiper: {},
+            slideToCurrent(el: HTMLElement) {
+                const sliderIndex = Number(el.dataset.sliderIndex!);
+                this.swiper.slideTo(sliderIndex, 300);
+            },
+            init() {
+                const swiperParams: SwiperOptions = {
+                    // configure Swiper to use modules
+                    modules: [Navigation],
+                    slidesPerView: "auto",
+                    spaceBetween: "24",
+                    loop: false,
+                    mousewheel: true,
+                    navigation: {
+                        nextEl: this.$refs.buttonNext,
+                        prevEl: this.$refs.buttonPrev,
+                        disabledClass: "opacity-0",
+                    },
+                };
+                this.swiper = new Swiper(
+                    this.$refs.swiperContainer,
+                    swiperParams
+                );
 
-const stickyMenu: HTMLElement = document.querySelector(".js-sticky-menu")!;
-const links: NodeListOf<HTMLElement> = stickyMenu?.querySelectorAll(
-    ".js-sticky-menu-item "
-);
-const activeClass = "is-active";
-const sectionOffset = 80;
+                this.$nextTick(() => {
+                    this.swiper.init();
+                });
 
-let isClicked = false;
-addEventListener("scroll", () => {
-    links.forEach((el) => {
-        const a: HTMLAnchorElement = el.querySelector("a")!;
-        const anchor: string = a.getAttribute("href")!;
-        const block: HTMLElement = document.querySelector(anchor)!;
-        const blockTop: number = block.offsetTop;
-        const sectionOffsetTop =
-            block.getBoundingClientRect().top + window.pageYOffset;
-        const sectionHeight = block.getBoundingClientRect().height;
-        const filterOffsetTop =
-            stickyMenu.getBoundingClientRect().top + window.pageYOffset;
+                const stickyMenu: HTMLElement =
+                    document.querySelector(".js-sticky-menu")!;
+                const links: NodeListOf<HTMLElement> =
+                    stickyMenu?.querySelectorAll(".js-sticky-menu-item ");
+                const activeClass = "is-active";
+                const sectionOffset = 80;
 
-        el.onclick = function (click: MouseEvent) {
-            isClicked = true;
-            links.forEach((allLinks) => {
-                allLinks.classList.remove(activeClass);
-            });
-            click.preventDefault();
-            window.scrollTo({
-                top: blockTop,
-                left: 0,
-                behavior: "smooth",
-            });
-            el.classList.add(activeClass);
+                let isClicked = false;
+                addEventListener("scroll", () => {
+                    links.forEach((el: HTMLElement) => {
+                        const a: HTMLAnchorElement = el.querySelector("a")!;
+                        const anchor: string = a.getAttribute("href")!;
+                        const block: HTMLElement =
+                            document.querySelector(anchor)!;
+                        const blockTop: number = block.offsetTop;
+                        const sectionOffsetTop: number =
+                            block.getBoundingClientRect().top +
+                            window.pageYOffset;
+                        const sectionHeight: number =
+                            block.getBoundingClientRect().height;
+                        const filterOffsetTop: number =
+                            stickyMenu.getBoundingClientRect().top +
+                            window.pageYOffset;
 
-            // ce timeout sert à ne pas jouer l'animation "is-active" lors du scrollTo
-            setTimeout(() => {
-                isClicked = false;
-            }, 500);
+                        el.onclick = (click: MouseEvent) => {
+                            isClicked = true;
+                            links.forEach((allLinks) => {
+                                allLinks.classList.remove(activeClass);
+                            });
+                            click.preventDefault();
+
+                            window.scrollTo({
+                                top: blockTop,
+                                left: 0,
+                                behavior: "smooth",
+                            });
+
+                            this.slideToCurrent(el);
+
+                            el.classList.add(activeClass);
+                            // ce timeout sert à ne pas jouer l'animation "is-active" lors du scrollTo
+                            setTimeout(() => {
+                                isClicked = false;
+                            }, 800);
+                        };
+
+                        // Partie qui gère la classe active lors du survol des blocks
+                        if (
+                            filterOffsetTop >
+                                sectionOffsetTop - sectionOffset &&
+                            !isClicked &&
+                            filterOffsetTop <
+                                sectionOffsetTop + sectionHeight - sectionOffset
+                        ) {
+                            const activeFilter = stickyMenu.querySelector(
+                                "." + activeClass
+                            );
+                            if (activeFilter) {
+                                activeFilter.classList.remove(activeClass);
+                            }
+                            if (!el.classList.contains(activeClass)) {
+                                el.classList.add(activeClass);
+                            }
+                            // Scroll vers l'item actif
+                            this.slideToCurrent(el);
+                        }
+                    });
+                });
+            },
         };
-
-        if (
-            filterOffsetTop > sectionOffsetTop - sectionOffset &&
-            !isClicked &&
-            filterOffsetTop < sectionOffsetTop + sectionHeight - sectionOffset
-        ) {
-            const activeFilter = stickyMenu.querySelector("." + activeClass);
-            if (activeFilter) {
-                activeFilter.classList.remove(activeClass);
-            }
-            if (!el.classList.contains(activeClass)) {
-                el.classList.add(activeClass);
-            }
-
-            const sliderIndex: any = el.dataset.sliderIndex!;
-
-            swiper.slideTo(sliderIndex, 300);
-        }
     });
 });
