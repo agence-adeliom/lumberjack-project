0dae459ac46c40d0e31f0a595dc2a83dd0c55391
Lucas Vigneron <81555425+LucasVigneronAdeliom@users.noreply.github.com>
2023-07-26 16:17:41 +0200

    feat(flex): re-order categories (#90)

--- web/app/themes/adeliom/app/Hooks/Admin/BlockHooks.php
+++ web/app/themes/adeliom/app/Hooks/Admin/BlockHooks.php
@@ -19,44 +19,9 @@ class BlockHooks
             $categories,
             [
                 [
-                    'slug' => GutBlockName::GENERIC,
-                    'title' => 'Texte et Images',
-                    'icon' => 'align-right',
-                ],
-                [
-                    'slug' => GutBlockName::LISTING,
-                    'title' => 'Listes',
-                    'icon' => 'editor-ul',
-                ],
-                [
-                    'slug' => GutBlockName::CTA,
-                    'title' => 'CTA',
-                    'icon' => 'button',
-                ],
-                [
-                    'slug' => GutBlockName::DOWNLOAD,
-                    'title' => 'Téléchargement',
-                    'icon' => 'download',
-                ],
-                [
-                    'slug' => GutBlockName::ACCORDIONS,
-                    'title' => 'Accordéons',
-                    'icon' => 'arrow-down-alt2',
-                ],
-                [
-                    'slug' => GutBlockName::TESTIMONIALS,
-                    'title' => 'Témoignages',
-                    'icon' => 'format-quote',
-                ],
-                [
-                    'slug' => GutBlockName::LATEST,
-                    'title' => 'Remontées automatiques',
-                    'icon' => 'page',
-                ],
-                [
-                    'slug' => GutBlockName::OTHERS,
-                    'title' => 'Autre',
-                    'icon' => 'marker',
+                    'slug' => GutBlockName::HERO,
+                    'title' => 'Header',
+                    'icon' => 'editor-kitchensink',
                 ],
                 [
                     'slug' => GutBlockName::NAVIGATION,
@@ -64,20 +29,55 @@ class BlockHooks
                     'icon' => 'menu',
                 ],
                 [
+                    'slug' => GutBlockName::CONTENT,
+                    'title' => 'Contenu',
+                    'icon' => 'admin-post',
+                ],
+                [
+                    'slug' => GutBlockName::GENERIC,
+                    'title' => 'Texte et Images',
+                    'icon' => 'align-right',
+                ],
+                [
+                    'slug' => GutBlockName::CTA,
+                    'title' => 'CTA',
+                    'icon' => 'button',
+                ],
+                [
+                    'slug' => GutBlockName::LISTING,
+                    'title' => 'Listes',
+                    'icon' => 'editor-ul',
+                ],
+                [
+                    'slug' => GutBlockName::LATEST,
+                    'title' => 'Remontées automatiques',
+                    'icon' => 'page',
+                ],
+                [
+                    'slug' => GutBlockName::ACCORDIONS,
+                    'title' => 'Accordéons',
+                    'icon' => 'arrow-down-alt2',
+                ],
+                [
                     'slug' => GutBlockName::RELATION,
                     'title' => 'Relation',
                     'icon' => 'networking',
                 ],
                 [
-                    'slug' => GutBlockName::HERO,
-                    'title' => 'Header',
-                    'icon' => 'editor-kitchensink',
+                    'slug' => GutBlockName::DOWNLOAD,
+                    'title' => 'Téléchargement',
+                    'icon' => 'download',
                 ],
                 [
-                    'slug' => GutBlockName::CONTENT,
-                    'title' => 'Contenu',
-                    'icon' => 'admin-post',
-                ]
+                    'slug' => GutBlockName::TESTIMONIALS,
+                    'title' => 'Témoignages',
+                    'icon' => 'format-quote',
+                ],
+                [
+                    'slug' => GutBlockName::OTHERS,
+                    'title' => 'Autre',
+                    'icon' => 'marker',
+                ],
             ]
         );
     }
